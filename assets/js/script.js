d4ecd8d4de6267ff172cf13cbbb577aa02942d44
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-08 19:42:14 +0530

    js: top button and hamburger functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,34 @@
-/* Author: 
+//Top Button Logic
+var topbtn = document.getElementsByClassName("topbtn")[0];
+topbtn.addEventListener("click", gotop);
 
-*/
+window.onscroll = scroll1;
 
+function scroll1() {
+    window.scrollY > 100 ? topbtn.style.display = 'Block' : topbtn.style.display = 'none';
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function gotop() {
+    var t1 = 0;
+    var y1 = window.scrollY;
+    y1 = y1 - 50;
+    window.scrollTo(0, y1);
+    if (y1 > 0) {
+        t1 = setTimeout("gotop()", 10);
+    } else {
+        clearTimeout(t1);
+    }
+}
 
 
+//Hamburger Function
+var ham = document.getElementsByClassName("menu-btn")[0];
+var hamfull = document.getElementById("menufull");
+var menunav = document.getElementsByClassName("menu-ul")[0];
+var body = document.querySelector("body");
 
+ham.addEventListener("click", function () {
+    menunav.classList.toggle("block");
+    ham.classList.toggle("active");
+    body.classList.toggle("bodyh");
+});
